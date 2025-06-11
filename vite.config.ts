4a37572ce116c2426f86756b5d3529c6685c2b3d
Innunekko <nitinc3114@gmail.com>
2025-06-11 16:48:30 +0530

    code editior

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,22 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: [
+      '@codemirror/state',
+      '@codemirror/view',
+      '@codemirror/language'
+    ],
+  },
   optimizeDeps: {
-    exclude: [
-      'lucide-react',
+    include: [
       '@codemirror/state',
       '@codemirror/view',
       '@codemirror/lang-javascript',
-      '@codemirror/lang-typescript',
-      '@codemirror/lang-dart',
-      '@codemirror/theme-one-dark',
-      '@codemirror/commands',
-      '@codemirror/search',
-      '@codemirror/autocomplete',
-      '@codemirror/lint'
+      '@codemirror/lang-yaml', 
+      '@codemirror/lang-markdown',
+      '@uiw/codemirror-theme-vscode',
+      '@uiw/react-codemirror'
     ],
   },
 });
